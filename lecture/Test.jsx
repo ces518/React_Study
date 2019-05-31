06c9a45fd50132453bd1ac1a987e55dc0edb9ab4
ces518 <pupupee9@gmail.com>
2019-05-31 14:05:44 +0900

    createRef, 1급함수

--- lecture/Test.jsx
+++ lecture/Test.jsx
@@ -1,8 +1,8 @@
-import React, { PureComponent, memo } from 'react';
+import React, { PureComponent, memo, createRef } from 'react';
 
 /*
 
-성능최적화 
+성능최적화
 
 PureComponent 리액트 불필요한 랜더링을 자동적으로 막아줌
 shouldComponentUpdate 자동구현
@@ -24,10 +24,35 @@ const TestHook = memo(() => {
     )
 })
 
+/*
+CreateRef: 엘리먼트의 참조를 자동적으로 생성가능함.
+
+ex) inputRef.current. 으로 접근해야한다.
+
+기존방식:
+메서드로 참조
+
+onInputRef = (c) => {
+    this.inputRef = c;
+}
+div ref={this.onInputRef}
+
+ */
+
+/*
+
+함수안에 다른 함수를 넣을수 있는경우 좀더 세밀한 조작이가능하다.
+1급함수 라고 말함
+자바스크립트는 20년전부터 지원함 ...
+
+ */
 class Test extends PureComponent {
+
+    inputRef = createRef();
+
     render() {
         return (
-            <div> hello , Test</div>
+            <div ref={this.inputRef}> hello , Test</div>
         )
     }
 }
