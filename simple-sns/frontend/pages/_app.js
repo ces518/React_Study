e45b9aa93b19bc9ec679b2fc9239b9b91169e2e4
ces518 <pupupee9@gmail.com>
2019-07-10 22:40:32 +0900

    SimpleSNS Project - redux 적용, next-redux-wrapper 활용 next - redux 연결

--- simple-sns/frontend/pages/_app.js
+++ simple-sns/frontend/pages/_app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
+
+import withRedux from 'next-redux-wrapper';
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
+
+import reducer from '../reducers';
+
 import Layout from '../components/Layout';
 
-const App = ({ Component }) => {
+const App = ({ Component, store }) => {
     return (
-        <>
+        <Provider store={store}>
             <Head>
                 <title>React-SNS</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"/>
@@ -14,7 +21,7 @@ const App = ({ Component }) => {
             <Layout>
                 <Component />
             </Layout>
-        </>
+        </Provider>
     )
 };
 
@@ -22,4 +29,7 @@ App.proptypes = {
     Component: PropTypes.elementType,
 };
 
-export default App;
+export default withRedux((initialState, options) => {
+    const store = createStore(reducer, initialState);
+    return store;
+})(App);
