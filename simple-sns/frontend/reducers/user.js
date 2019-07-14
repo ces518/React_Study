a6986ae4e4a40a3e3edfb824a1640dc578f0b557
ces518 <pupupee9@gmail.com>
2019-07-14 22:39:27 +0900

    SimpleSNS Project - dummy user 정의, login action, logout action 정의, login reducer, logout reducer 작성

--- simple-sns/frontend/reducers/user.js
+++ simple-sns/frontend/reducers/user.js
@@ -1,13 +1,59 @@
 export const initialState = {
-    info: {
-        id: 0,
-        nickname: '준영',
-    }, // 로그인 정보
+    info: null, // 로그인 정보
+    isLoggingIn: false, //로그인 시도중 상태값
+
 };
 
+const dummyUser = {
+    id: 0,
+    nickname: '준영',
+};
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
 
 const reducer = (state = initialState, action) => {
-    switch (action) {
+    switch (action.type) {
+        case LOGIN_REQUEST: {
+            return {
+                ...state,
+                isLoggingIn: true,
+            }
+        }
+        case LOGIN_SUCCESS: {
+            return {
+                ...state,
+                isLoggingIn: false,
+                info: dummyUser,
+            }
+        }
+        case LOGIN_FAILURE: {
+            return {
+                ...state,
+                isLoggingIn: false,
+            }
+        }
+        case LOGOUT_REQUEST: {
+            return {
+                ...state,
+            }
+        }
+        case LOGOUT_SUCCESS: {
+            return {
+                ...state,
+                info: null,
+            }
+        }
+        case LOGOUT_FAILURE: {
+            return {
+                ...state,
+            }
+        }
         default: {
             return {
                 ...state,
