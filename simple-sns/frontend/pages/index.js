8e5b084a3131ecbbcc6a2c44248a7daddcdba03c
ces518 <pupupee9@gmail.com>
2019-07-23 23:32:19 +0900

    SimpleSNS Project - 더미데이터 활용한 게시글 목록 구현

--- simple-sns/frontend/pages/index.js
+++ simple-sns/frontend/pages/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { useSelector } from "react-redux";
 
 import PostForm from '../components/PostForm';
+import PostCard from "../components/PostCard";
 
 const Home = () => {
     const { info } = useSelector(state => state.user);
+    const { posts } = useSelector(state => state.post);
+    console.dir(posts);
     return (
         <>
             {info ?
                 <PostForm />
                 : null
             }
+            {posts.map(post => <PostCard post={post} key={post.id}/>)}
         </>
     )
 };
